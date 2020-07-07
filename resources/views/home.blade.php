9000e4d5179993b14517b01c0a70534b81128891
Mark Salmon <marksalmon@mark.salmon@mediconesystems.com>
2020-07-07 09:51:42 +0100

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
 
     <h1 class="text-3xl text-gray-700">Datatable of Contents</h1>
 
-    <livewire:datatable-of-contents />
+    <livewire:datatable-of-contents hide-header hide-pagination />
 
     <x-code file="app/Http/Livewire/DatatableOfContents.php" />
 
